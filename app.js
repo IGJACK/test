fe953fb0be0b10fcc1541583deca01f083679eca
zxl <a@b.com>
2019-05-29 16:20:36 +0800

    添加demo路由

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.listen(3000);
+app.get('/index',(req,res)=>{
+    res.send('');
+});
+app.get('/demon',(req,res)=>{
+    res.send('');
+});
+
